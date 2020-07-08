ba2cfc697bfd0edc13789618461181405fe7d110
Sean Reid <sean.reid8@gmail.com>
2020-07-08 18:47:31 -0400

    first implementation

--- src/main/java/com/idtus/sreid/threadpools/ResponsiveScheduledThreadPool.java
+++ src/main/java/com/idtus/sreid/threadpools/ResponsiveScheduledThreadPool.java
@@ -15,7 +15,8 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicLong;
+import javax.annotation.Nonnull;
+import org.apache.commons.lang3.mutable.MutableObject;
 
 /**
  * A {@link ScheduledExecutorService} that guarantees that a given task will
@@ -35,36 +36,28 @@ public class ResponsiveScheduledThreadPool implements ScheduledExecutorService {
   private final transient ExecutorService delegateExecutorService;
 
   /**
-   * The unit-less time to wait between checking to see if
-   * any tasks are ready to start.
-   */
-  private final transient long waitTimeScalar;
-
-  /**
-   * The time unit to interpret {@link #waitTimeScalar} as.
-   */
-  private final transient TimeUnit waitTimeUnit;
-
-  /**
    * The queue used to store work before it's started.
    */
-  private final transient PriorityBlockingQueue<Callable<?>> workQueue;
+  private final transient PriorityBlockingQueue<ScheduledFutureImpl<?>> workQueue;
 
   /**
    * The constructor.
    * @param threadFactory the factory to use to make threads in the underlying executor
-   * @param waitTimeScalar the time to wait between polling the work queue
+   * @param waitTimeScalar the time to wait between polling the work queue.
+   *                       If this value is less than 0,
+   *                       the absolute value of the provided value is used.
    * @param waitTimeUnit the unit of the polling time
    */
   public ResponsiveScheduledThreadPool(
       final ThreadFactory threadFactory,
       final long waitTimeScalar,
-      final TimeUnit waitTimeUnit) {
+      final TimeUnit waitTimeUnit,
+      final String poolName) {
 
     Preconditions.checkNotNull(waitTimeUnit);
-
-    this.waitTimeScalar = waitTimeScalar;
-    this.waitTimeUnit = waitTimeUnit;
+    if (waitTimeScalar <= 0) {
+      throw new IllegalArgumentException("Wait time cannot be 0 or less.");
+    }
 
     this.delegateExecutorService = Executors.newCachedThreadPool(threadFactory);
 
@@ -72,133 +65,551 @@ public class ResponsiveScheduledThreadPool implements ScheduledExecutorService {
 
     this.managementThread = new Thread(
         new ManagementRunnable(
-            TimeUnit.MILLISECONDS.convert(
+            TimeUnit.NANOSECONDS.convert(
                 Math.abs(waitTimeScalar),
                 waitTimeUnit)));
-    this.managementThread.setPriority(Thread.NORM_PRIORITY);
-    this.managementThread.setName(this.getClass().getName() + " Management Thread");
+    this.managementThread.setPriority(Thread.MAX_PRIORITY);
+    this.managementThread.setName(this.getClass().getName() + " " + poolName + " Management Thread");
     this.managementThread.setDaemon(true);
     this.managementThread.start();
 
   }
 
+  @Override
+  @Nonnull
+  public ScheduledFuture<?> schedule(
+      final @Nonnull Runnable runnable,
+      final long delay,
+      final @Nonnull TimeUnit unit) {
 
-  public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
-    return null;
+    final Callable<?> callable = Executors.callable(runnable);
+    return schedule(callable, delay, unit);
   }
 
-  public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
-    return null;
+  @Override
+  @Nonnull
+  public <V> ScheduledFuture<V> schedule(
+      final @Nonnull Callable<V> callable,
+      final long delay,
+      final @Nonnull TimeUnit unit) {
+
+    final ScheduledFutureImpl<V> future = new ScheduledFutureImpl<>(delay, unit, callable);
+    workQueue.add(future);
+    return future;
   }
 
-  public ScheduledFuture<?> scheduleAtFixedRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
-    return null;
+  @Override
+  @Nonnull
+  public ScheduledFuture<?> scheduleAtFixedRate(
+      final @Nonnull Runnable runnable,
+      final long initialDelay,
+      final long period,
+      final @Nonnull TimeUnit unit) {
+
+
+    final Callable<?> callable =
+        new FixedRateCallable(
+            runnable,
+            period,
+            unit);
+
+    ScheduledFutureImpl<?> future =
+        new ScheduledFutureImpl<>(
+            initialDelay,
+            unit,
+            callable);
+    workQueue.add(future);
+    return future;
   }
 
-  public ScheduledFuture<?> scheduleWithFixedDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
-    return null;
+  @Override
+  @Nonnull
+  public ScheduledFuture<?> scheduleWithFixedDelay(
+      final @Nonnull Runnable runnable,
+      final long initialDelay,
+      final long delay,
+      final @Nonnull TimeUnit unit) {
+
+    final Callable<?> callable =
+        new FixedDelayCallable(
+            runnable,
+            delay,
+            unit);
+
+    ScheduledFutureImpl<?> future =
+        new ScheduledFutureImpl<>(
+            initialDelay,
+            unit,
+            callable);
+
+    workQueue.add(future);
+    return future;
   }
 
+  @Override
   public void shutdown() {
-
+    this.managementThread.interrupt();
+    this.delegateExecutorService.shutdown();
   }
 
+  @Override
+  @Nonnull
   public List<Runnable> shutdownNow() {
-    return null;
+    this.managementThread.interrupt();
+    return this.delegateExecutorService.shutdownNow();
   }
 
+  @Override
   public boolean isShutdown() {
-    return false;
+    return
+        (this.managementThread.isInterrupted()
+            || !this.managementThread.isAlive())
+        && this.delegateExecutorService.isShutdown();
   }
 
+  @Override
   public boolean isTerminated() {
-    return false;
+    return this.delegateExecutorService.isTerminated();
   }
 
-  public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
-    return false;
+  @Override
+  public boolean awaitTermination(
+      final long timeout,
+      final @Nonnull TimeUnit unit)
+      throws InterruptedException {
+
+    // don't schedule any more tasks
+    this.managementThread.interrupt();
+    return this.delegateExecutorService.awaitTermination(timeout, unit);
   }
 
-  public <T> Future<T> submit(Callable<T> task) {
-    return null;
+  @Override
+  @Nonnull
+  public <T> Future<T> submit(
+      final @Nonnull Callable<T> task) {
+
+    return this.delegateExecutorService.submit(task);
   }
 
-  public <T> Future<T> submit(Runnable task, T result) {
-    return null;
+  @Override
+  @Nonnull
+  public <T> Future<T> submit(
+      final @Nonnull Runnable task,
+      final T result) {
+
+    return this.delegateExecutorService.submit(task, result);
   }
 
-  public Future<?> submit(Runnable task) {
-    return null;
+  @Override
+  @Nonnull
+  public Future<?> submit(
+      final @Nonnull Runnable task) {
+
+    return this.delegateExecutorService.submit(task);
   }
 
-  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks) throws InterruptedException {
-    return null;
+  @Override
+  @Nonnull
+  public <T> List<Future<T>> invokeAll(
+      @Nonnull final Collection<? extends Callable<T>> tasks)
+      throws InterruptedException {
+
+    return this.delegateExecutorService.invokeAll(tasks);
   }
 
-  public <T> List<Future<T>> invokeAll(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException {
-    return null;
+  @Override
+  @Nonnull
+  public <T> List<Future<T>> invokeAll(
+      final @Nonnull Collection<? extends Callable<T>> tasks,
+      final long timeout,
+      final @Nonnull TimeUnit unit)
+      throws InterruptedException {
+
+    return this.delegateExecutorService.invokeAll(tasks, timeout, unit);
   }
 
-  public <T> T invokeAny(Collection<? extends Callable<T>> tasks) throws InterruptedException, ExecutionException {
-    return null;
+  @Override
+  @Nonnull
+  public <T> T invokeAny(
+      final @Nonnull Collection<? extends Callable<T>> tasks)
+      throws InterruptedException, ExecutionException {
+
+    return this.delegateExecutorService.invokeAny(tasks);
   }
 
-  public <T> T invokeAny(Collection<? extends Callable<T>> tasks, long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
-    return null;
+  @Override
+  public <T> T invokeAny(
+      final @Nonnull Collection<? extends Callable<T>> tasks,
+      final long timeout,
+      final @Nonnull TimeUnit unit)
+      throws InterruptedException, ExecutionException, TimeoutException {
+
+    return this.delegateExecutorService.invokeAny(tasks, timeout, unit);
   }
 
-  public void execute(Runnable command) {
+  @Override
+  public void execute(
+      final @Nonnull Runnable command) {
 
+    this.delegateExecutorService.execute(command);
   }
 
+  /**
+   * The runnable that manages transitioning scheduled tasks into
+   * executing tasks on time.
+   */
   private final class ManagementRunnable implements Runnable {
-    private final long waitDurationMillis;
 
-    public ManagementRunnable(final long waitDurationMillis) {
-      this.waitDurationMillis = Math.abs(waitDurationMillis);
+    /**
+     * The amount of time to sleep between attempts
+     * to begin tasks who's scheduled time has come, in milliseconds.
+     */
+    private final transient long waitDurationMillis;
+
+    /**
+     * Any additional time to sleep after the sleep millis has passed, in nanos.
+     */
+    private final transient int waitDurationNanos;
+
+    /**
+     * Constructor.
+     * @param waitDurationNanos the time in nano seconds to wait between
+     *                          attempts to start scheduled tasks.
+     */
+    public ManagementRunnable(final long waitDurationNanos) {
+      final long durationNanos = Math.abs(waitDurationNanos);
+      this.waitDurationMillis =
+          TimeUnit.MILLISECONDS.convert(
+              durationNanos,
+              TimeUnit.NANOSECONDS);
+      this.waitDurationNanos = (int)
+          (durationNanos - TimeUnit.NANOSECONDS.convert(
+              this.waitDurationMillis,
+              TimeUnit.MILLISECONDS));
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void run() {
-
-
+      while (!Thread.currentThread().isInterrupted()) {
+        while (workQueue.peek() != null
+            && System.nanoTime() >= workQueue.peek().getExecutionTime()) {
+          final ScheduledFutureImpl<?> scheduledFuture = workQueue.poll();
+          if (scheduledFuture != null) {
+            synchronized (scheduledFuture) {
+              final Future future =
+                  delegateExecutorService.submit(scheduledFuture.getCallable());
+              scheduledFuture.setDelegateFuture(future);
+            }
+          }
+        }
+        try {
+          Thread.sleep(
+              this.waitDurationMillis,
+              this.waitDurationNanos);
+        } catch (final InterruptedException e) {
+          Thread.currentThread().interrupt();
+        }
+      }
     }
   }
 
-  private final class ScheduleFutureImpl<V> implements ScheduledFuture<V> {
+  /**
+   * An implementation of {@link ScheduledFuture}.
+   * @param <V> the expected return type
+   */
+  private final class ScheduledFutureImpl<V> implements ScheduledFuture<V> {
+
+    /**
+     * The object that stores the delegate future.
+     */
+    private final transient MutableObject<Future<V>> delegateFuture;
+
+    /**
+     * Whether or not this future has been cancelled.
+     */
+    private volatile transient boolean cancelled;
+
+    /**
+     * The number of nanoseconds to delay before executing.
+     */
+    private final transient long delayNanos;
+
+    /**
+     * The nanosecond time at which to begin execution.
+     */
+    private final transient long executionTime;
+
+    /**
+     * The callable to run when the delay has expired.
+     */
+    private final transient Callable<V> callable;
+
+
+    public ScheduledFutureImpl(
+        final long delay,
+        final TimeUnit unit,
+        final Callable<V> callable) {
+
+      this.delegateFuture = new MutableObject<>(null);
+      this.cancelled = false;
+      this.delayNanos = TimeUnit.NANOSECONDS.convert(delay, unit);
+      this.executionTime = System.nanoTime() + this.delayNanos;
+      this.callable = callable;
+    }
+
+    /**
+     * Sets the true future once the task has begun actually executing.
+     * @param future the true future
+     */
+    private void setDelegateFuture(final Future<V> future) {
+      synchronized (this) {
+        synchronized (this.delegateFuture) {
+          this.delegateFuture.setValue(future);
+        }
+        this.notifyAll();
+      }
+    }
+
+    /**
+     * Gets the time at which to execute this task.
+     * @return the nanosecond time at which to execute this task
+     */
+    private long getExecutionTime() {
+      return this.executionTime;
+    }
+
+    /**
+     * Returns the {@link Callable} to run when the scheduled time comes.
+     * @return the callable
+     */
+    private Callable<V> getCallable() {
+      return this.callable;
+    }
 
     @Override
     public long getDelay(TimeUnit unit) {
-      return 0;
+      return unit.convert(delayNanos, TimeUnit.NANOSECONDS);
     }
 
     @Override
     public int compareTo(Delayed o) {
-      return 0;
+      if (o instanceof ScheduledFutureImpl<?>) {
+        final long difference =
+            this.executionTime
+                - ((ScheduledFutureImpl<?>) o).getExecutionTime();
+        if (difference < 0) {
+          return -1;
+        }
+        if (difference > 0) {
+          return 1;
+        }
+        return 0;
+      }
+      return (int)(this.delayNanos - o.getDelay(TimeUnit.NANOSECONDS));
     }
 
     @Override
     public boolean cancel(boolean mayInterruptIfRunning) {
-      return false;
+      final boolean delegateFuturePresent;
+      synchronized (this.delegateFuture) {
+        delegateFuturePresent = this.delegateFuture.getValue() != null;
+      }
+      if (!delegateFuturePresent) {
+        workQueue.remove(this);
+        this.cancelled = true;
+      } else {
+        synchronized (this.delegateFuture) {
+          this.cancelled = this.delegateFuture.getValue().cancel(mayInterruptIfRunning);
+        }
+      }
+      return this.cancelled;
     }
 
     @Override
     public boolean isCancelled() {
-      return false;
+      final boolean delegateFuturePresent;
+      synchronized (this.delegateFuture) {
+        delegateFuturePresent = this.delegateFuture.getValue() != null;
+      }
+      if (!delegateFuturePresent) {
+        return this.cancelled;
+      }
+      synchronized (this.delegateFuture) {
+        return this.delegateFuture.getValue().isCancelled();
+      }
     }
 
     @Override
     public boolean isDone() {
-      return false;
+      final boolean delegateFuturePresent;
+      synchronized (this.delegateFuture) {
+        delegateFuturePresent = this.delegateFuture.getValue() != null;
+      }
+      if (!delegateFuturePresent) {
+        return false;
+      }
+      synchronized (this.delegateFuture) {
+        return this.delegateFuture.getValue().isDone();
+      }
     }
 
     @Override
     public V get() throws InterruptedException, ExecutionException {
-      return null;
+      final boolean delegateFuturePresent;
+      synchronized (this.delegateFuture) {
+        delegateFuturePresent = this.delegateFuture.getValue() != null;
+      }
+      if (!delegateFuturePresent) {
+        synchronized (this) {
+          this.wait();
+        }
+      }
+      final Future<V> delegate;
+      synchronized (this.delegateFuture) {
+        delegate = this.delegateFuture.getValue();
+      }
+      return delegate.get();
     }
 
     @Override
-    public V get(long timeout, TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+    public V get(
+        final long timeout,
+        final @Nonnull TimeUnit unit)
+        throws InterruptedException, ExecutionException, TimeoutException {
+
+      final boolean delegateFuturePresent;
+      synchronized (this.delegateFuture) {
+        delegateFuturePresent = this.delegateFuture.getValue() != null;
+      }
+
+      final long start = System.nanoTime();
+      long remaining = 0;
+      if (!delegateFuturePresent) {
+        synchronized (this) {
+          this.wait(TimeUnit.MILLISECONDS.convert(timeout, unit));
+        }
+        final long elapsed =
+            System.nanoTime() - start;
+
+        remaining = TimeUnit.NANOSECONDS.convert(timeout, unit) - elapsed;
+        if (elapsed >= TimeUnit.NANOSECONDS.convert(timeout, unit) ) {
+          throw new TimeoutException("Result unavailable before timeout");
+        }
+      }
+
+      final Future<V> delegate;
+      synchronized (this.delegateFuture) {
+        delegate = this.delegateFuture.getValue();
+      }
+      return delegate.get(remaining, TimeUnit.NANOSECONDS);
+    }
+  }
+
+  /**
+   * A callable that reschedules itself on a periodic.
+   */
+  private final class FixedRateCallable implements Callable<Void> {
+
+    /**
+     * The runnable to run.
+     */
+    private final transient Runnable runnable;
+
+    /**
+     * The period on which to reschedule self, in nanoseconds.
+     */
+    private final transient long periodNanos;
+
+    /**
+     * Constructor.
+     * @param runnable the runnable
+     * @param period the period to reschedule self
+     * @param unit the unit of te period
+     */
+    public FixedRateCallable(
+        final @Nonnull Runnable runnable,
+        final long period,
+        final @Nonnull TimeUnit unit) {
+
+      this.runnable = runnable;
+      this.periodNanos = TimeUnit.NANOSECONDS.convert(period, unit);
+    }
+
+    @Override
+    public Void call() {
+      final FixedRateCallable newSelf =
+          new FixedRateCallable(
+              this.runnable,
+              this.periodNanos,
+              TimeUnit.NANOSECONDS);
+
+      final ScheduledFuture<?> next =
+          schedule(
+              newSelf,
+              this.periodNanos,
+              TimeUnit.NANOSECONDS);
+
+      try {
+        this.runnable.run();
+      } catch (final Throwable e) {
+        // cancel future iterations if an exception occurs
+        next.cancel(/* cancel if it's already running */ true);
+        throw e;
+      }
+      return null;
+    }
+  }
+
+  /**
+   * A callable that reschedules itself to run after the previous iteration of
+   * itself has run.
+   */
+  private final class FixedDelayCallable implements Callable<Void> {
+
+    /**
+     * The runnable to run.
+     */
+    private final transient Runnable runnable;
+
+    /**
+     * The delay before the task will run again, in nanoseconds.
+     */
+    private final transient long delay;
+
+    public FixedDelayCallable(
+        final @Nonnull Runnable runnable,
+        final long delay,
+        final @Nonnull TimeUnit unit) {
+
+      this.runnable = runnable;
+      this.delay = TimeUnit.NANOSECONDS.convert(delay, unit);
+    }
+
+    @Override
+    public Void call() {
+
+      try {
+        this.runnable.run();
+      } catch (final Throwable e) {
+        // don't schedule again if anything went wrong.
+        return null;
+      }
+
+      final FixedDelayCallable newSelf =
+          new FixedDelayCallable(
+              this.runnable,
+              this.delay,
+              TimeUnit.NANOSECONDS);
+
+      final ScheduledFutureImpl<?> future =
+          new ScheduledFutureImpl<>(
+              this.delay,
+              TimeUnit.NANOSECONDS,
+              newSelf);
+
+      workQueue.add(future);
+
       return null;
     }
   }
---
+++ src/test/java/com/idtus/sreid/threadpools/ResponsiveScheduledThreadPoolTest.java
@@ -0,0 +1,40 @@
+package com.idtus.sreid.threadpools;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.junit.jupiter.api.Test;
+
+class ResponsiveScheduledThreadPoolTest {
+
+  @Test
+  public void testSchedule() throws Exception {
+    final ResponsiveScheduledThreadPool pool =
+        new ResponsiveScheduledThreadPool(
+            Thread::new,
+            1,
+            TimeUnit.NANOSECONDS,
+            "Test pool");
+
+    final Callable<Integer> callable = () -> 5;
+
+    final ScheduledFuture<Integer> future =
+        pool.schedule(callable, 5, TimeUnit.SECONDS);
+
+    assertThrows(
+        TimeoutException.class,
+        () -> future.get(1, TimeUnit.MILLISECONDS),
+        "Future should not be resolvable after 1ms");
+
+    Thread.sleep(5000);
+
+    final Integer result = future.get(1, TimeUnit.MILLISECONDS);
+    assertEquals(5, result, "Result should be 5");
+
+  }
+
+}
